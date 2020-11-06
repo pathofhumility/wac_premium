d41d9b6838f591c9c0e14c43593e2f4d97b8187e
Andy Holmes <beawareherenow@gmail.com>
2020-11-06 15:59:43 -0500

    daily update

--- coreLangaugeFeatures/quizThisPart3.js
+++ coreLangaugeFeatures/quizThisPart3.js
@@ -19,24 +19,27 @@ var person3 = personMakerC('person 3');
 
 // Which of the following statements are true?
 //
-// Q1. When paused at line 2, `this` is equal to PersonMakerA.
+// Q1. When paused at line 5, `this` is equal to PersonMakerA.
+//   a. False 100%
+//   b. Evidence: Debugger shows `this` referencing an object with property __prototype__
+// Q2. When paused at 18, column 40, person1, person2, and person3 are all objects.
+//   a. False 100%
+//   b. Evidence: Debugger shows person3 to be undefined.
+// Q3. The right side of line 16 returns an object with a name property.
 //   a. True 100%
-//   b. Evidence:
-// Q2. When paused at 15, column 40, person1, person2, and person3 are all objects.
-//   a.
-//   b. Evidence:
-// Q3. The right side of line 13 returns an object with a name property.
-//   a.
-//   b. Evidence:
-// Q4. The right side of line 14 returns an object with a name property.
-//   a.
-//   b. Evidence:
-// Q5. The right side of line 15 returns an object with a name property.
-//   a.
-//   b. Evidence:
+//   b. Evidence: The debugger shows person1 having a property of name. Checking person1
+//      with typeof indicates object. 
+// Q4. The right side of line 17 returns an object with a name property.
+//   a. True 100%
+//   b. Evidence: The dubugger shows person2 having a value of an object that has a name property.
+// Q5. The right side of line 18 returns an object with a name property.
+//   a. False 100%
+//   b. Evidence: The bugger shows person3 being undefined after the personMakerC function is
+//      called. This makes sense as that function does not have an explicit return.
 // Q6. Each time you call PersonMakerA with the new keyword, `this` inside of PersonMakerA will initially be an object with just one property, __proto__.
-//   a.
-//   b. Evidence:
+//   a.  True 100%
+//   b. Evidence: This is what the debugger shows.
 // Q7. When you call PersonMakerA with the new keyword, `this` inside of PersonMakerA will be returned by the function.
-//   a.
-//   b. Evidence:
+//   a. True 100%
+//   b. Evidence: The debugger shows the value of this after line 5 is executed === person1
+//      after the personMakerA function is called.
